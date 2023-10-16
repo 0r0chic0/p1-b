8c8a92c15bea49925815c400b6214890806ea91e
d_zhe <dzhen2021@gmail.com>
2023-10-15 21:15:40 -0700

    Wrote getInstance for SinusoidalWave

---
+++ CONTRIBS-dzhen2023.md
@@ -0,0 +1,1 @@
+- Wrote rep invariant and abstraction function for ConcreteSoundWave and implemented the constructor and getChannel methods as well as duration
--- src/main/java/cpen221/soundwaves/SinusoidalWave.java
+++ src/main/java/cpen221/soundwaves/SinusoidalWave.java
@@ -1,7 +1,11 @@
 package cpen221.soundwaves;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SinusoidalWave extends ConcreteSoundWave {
+    public static final double SECONDS_PER_SAMPLE = 1.0 / SAMPLES_PER_SECOND;
 
     /**
      * A private constructor
@@ -16,16 +20,22 @@ public class SinusoidalWave extends ConcreteSoundWave {
      * Obtain a new {@code SinusoidalWave} instance.
      *
      * @param freq      the frequency of the wave, > 0
-     * @param phase     the phase of the wave, >= 0
+     * @param phase     the phase of the wave in seconds, >= 0
      * @param amplitude the amplitude of the wave, is in (0, 1]
-     * @param duration  the duration of the wave, >= 0
+     * @param duration  the duration of the wave in seconds, >= 0
      * @return a {@code SinusoidalWave} instance with the specified parameters
      */
     public static SinusoidalWave getInstance(double freq, double phase, double amplitude,
                                              double duration) {
-        // TODO: Implement this method
-        // Some code has been completed to give you a sense of what to do
-        double[] channel = null;
+        int samples = (int) (duration * SAMPLES_PER_SECOND);
+        double[] channel = new double[samples];
+        double angFreq = 2 * Math.PI * freq;
+        double time = 0;
+
+        for (int i = 0; i < samples; time += SECONDS_PER_SAMPLE, i++) {
+            channel[i] = amplitude * Math.sin(angFreq * time + phase);
+        }
+
         return new SinusoidalWave(channel);
     }
 
