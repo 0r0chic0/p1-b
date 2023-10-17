fab94a14597ad5db1fa2ecea43fd99a43a422323
NimeshPandey <nimeshpnd14@gmail.com>
2023-10-17 13:35:09 -0700

    triangle wave

--- src/main/java/cpen221/soundwaves/TriangleWave.java
+++ src/main/java/cpen221/soundwaves/TriangleWave.java
@@ -1,12 +1,14 @@
 package cpen221.soundwaves;
 
 public class TriangleWave extends ConcreteSoundWave {
+    public static final double SECONDS_PER_SAMPLE = 1.0 / SAMPLES_PER_SECOND;
     /**
      * A private constructor
      *
      * @param channel the time series of amplitude values, is not null
      */
-    private TriangleWave(double[] channel) {
+    private TriangleWave(double[] channel)
+    {
         super(channel, channel);
     }
 
@@ -21,8 +23,16 @@ public class TriangleWave extends ConcreteSoundWave {
      */
     public static TriangleWave getInstance(double freq, double phase, double amplitude,
                                            double duration) {
-        // TODO: Implement this method
-        return null;
+        int samples = (int) (duration * SAMPLES_PER_SECOND);
+        double[] channel = new double[samples];
+        double angFreq = 2 * Math.PI * freq;
+        double time = 0;
+
+        for (int i = 0; i < samples; time += SECONDS_PER_SAMPLE, i++) {
+            channel[i] = 2* amplitude * freq * (1/(2*freq) * Math.abs((time + phase) % (1/freq) - (1/(2*freq))));
+        }
+
+        return new TriangleWave(channel);
     }
 
 }
